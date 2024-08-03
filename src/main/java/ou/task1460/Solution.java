72b2e8271bcf9c396828391947794dd4944999c8
EvgeniyMsk <logvinov.evgeniy@ya.ru>
2024-08-03 19:26:17 +0300

    1460 solved

---
+++ src/main/java/ou/task1460/Solution.java
@@ -0,0 +1,11 @@
+package ou.task1460;
+
+import java.util.Arrays;
+
+public class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        Arrays.sort(target);
+        Arrays.sort(arr);
+        return Arrays.equals(target, arr);
+    }
+}
