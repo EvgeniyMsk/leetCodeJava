82cd1ac4107f4d50b0a3d258df6d5256b69b5a42
EvgeniyMsk <logvinov.evgeniy@ya.ru>
2024-08-17 01:47:34 +1000

    119 solved

---
+++ src/test/java/ou/task119/TestApp.java
@@ -0,0 +1,12 @@
+package ou.task119;
+
+import org.junit.jupiter.api.Test;
+
+public class TestApp {
+    private final Solution solution = new Solution();
+
+    @Test
+    public void test_1() {
+        System.out.println(solution.getRow(3));
+    }
+}
